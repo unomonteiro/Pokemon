3a6d907aa8149c65fc3c9065f5b8ea540c3dc2e6
Carlos Monteiro <offbyte@gmail.com>
2018-09-23 23:25:15 +0100

    Catch pokemons

--- app/src/main/java/io/monteirodev/pokemon/MapsActivity.kt
+++ app/src/main/java/io/monteirodev/pokemon/MapsActivity.kt
@@ -13,7 +13,7 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
-import com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom
+import com.google.android.gms.maps.CameraUpdateFactory.newLatLng
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -26,7 +26,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     val LOCATION_REQUEST = 1
+    var oldLocation:Location?=null
     var location:Location?= null
+    var pokemonList = ArrayList<Pokemon>()
+    var playerPower = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         checkPermission()
+        loadPokemons()
     }
 
     /**
@@ -70,13 +74,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun getUserLocation() {
         Toast.makeText(this, "User locations access on", LENGTH_LONG).show()
 
-        var myLocation = MyLocationListener()
+        val myLocation = MyLocationListener()
 
-        var locationManager =
+        val locationManager =
                 getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         // checkPermission()
-        locationManager.requestLocationUpdates(GPS_PROVIDER, 3, 3f, myLocation)
+        try {
+            locationManager.requestLocationUpdates(GPS_PROVIDER, 1000, 3f, myLocation)
+        } catch (e: SecurityException) {
+            Toast.makeText(this, "permission location failed!", LENGTH_LONG).show()
+        }
 
         MyThread().start()
 
@@ -100,7 +108,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     inner class MyLocationListener() :LocationListener {
 
         init {
-            location = Location("start")
+            location = Location("Start")
             location!!.latitude = 0.0
             location!!.longitude = 0.0
         }
@@ -117,21 +125,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-    inner class MyThread : Thread() {
+    inner class MyThread() : Thread() {
+        init {
+            oldLocation = Location("Start")
+            oldLocation!!.latitude = 0.0
+            oldLocation!!.longitude = 0.0
+        }
+
         override fun run() {
             while (true) {
 // Add a marker in Sydney and move the camera
                 try {
+                    if (oldLocation!!.distanceTo(location) == 0f) {
+                        continue
+                    }
+                    oldLocation = location
+
                     runOnUiThread {
                         mMap.clear()
-                        val place = LatLng(location!!.latitude, location!!.longitude)
-                        mMap.addMarker(MarkerOptions()
-                                .position(place)
-                                .title("Me")
-                                .snippet("Here's me :)")
-                                .icon(fromResource(mario)))
-
-                        mMap.moveCamera(newLatLngZoom(place,10f))
+                        showMe()
+                        showPokemons()
                     }
 
                     Thread.sleep(1000)
@@ -141,5 +154,53 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+
+        fun showMe() {
+            val meLocation = LatLng(location!!.latitude, location!!.longitude)
+            mMap.addMarker(MarkerOptions()
+                    .position(meLocation)
+                    .title("Me")
+                    .snippet("Here's me :)")
+                    .icon(fromResource(mario)))
+            mMap.moveCamera(newLatLng(meLocation))
+        }
+
+        fun showPokemons() {
+            pokemonList.forEachIndexed { index, pokemon ->
+                if (!pokemon.isCaptured) {
+                    mMap.addMarker(MarkerOptions()
+                            .position(pokemon.latLng)
+                            .title(pokemon.name + " ${pokemon.power}")
+                            .snippet(pokemon.description)
+                            .icon(fromResource(pokemon.image)))
+                }
+                var pokemonLocation = Location(pokemon.name)
+                pokemonLocation.latitude = pokemon.latLng.latitude
+                pokemonLocation.longitude = pokemon.latLng.longitude
+                if (location!!.distanceTo(pokemonLocation) < 3f) {
+                    pokemon.isCaptured = true
+                    pokemonList[index] = pokemon
+                    playerPower += pokemon.power
+                    Toast.makeText(applicationContext,
+                            "You got ${pokemon.name} your power is $playerPower",
+                            LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+
+    fun loadPokemons() {
+        pokemonList.add(Pokemon(
+                "Charmander", "Charmander living in japan",
+                R.drawable.charmander,55.0,
+                LatLng(37.7789994893035, -122.401846647263),false))
+        pokemonList.add(Pokemon(
+                "Bulbasaur", "Bulbasaur living in usa",
+                R.drawable.bulbasaur,90.5,
+                LatLng(37.7949568502667, -122.410494089127), false))
+        pokemonList.add(Pokemon("Squirtle", "Squirtle living in iraq",
+                R.drawable.squirtle,33.5,
+                LatLng(37.7816621152613, -122.41225361824), false))
     }
 }
--- app/src/main/java/io/monteirodev/pokemon/Pokemon.kt
+++ app/src/main/java/io/monteirodev/pokemon/Pokemon.kt
@@ -1,2 +1,7 @@
 package io.monteirodev.pokemon
 
+import android.location.Location
+import com.google.android.gms.maps.model.LatLng
+
+class Pokemon(var name:String, var description:String, var image:Int, var power:Double,
+              var latLng: LatLng, var isCaptured:Boolean)
